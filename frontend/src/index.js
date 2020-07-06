624531fbdf8c51c9c2614629f98a25ede5f8e605
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-06 19:11:56 -0300

    Adicionado sass.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/main.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
