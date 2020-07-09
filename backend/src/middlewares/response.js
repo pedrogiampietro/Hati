bd4bdb81a9bbc5eca8d64fc3b7035a3bd13c25d1
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-08 21:15:26 -0300

    adicionado middlewares.

---
+++ backend/src/middlewares/response.js
@@ -0,0 +1,84 @@
+const TYPE_JSON = 'application/json'
+const STATUS_CODE_OK = 200
+const STATUS_CODE_BAD_REQUEST = 400
+const STATUS_CODE_UNAUTHORIZED = 401
+const STATUS_CODE_NOT_FOUND = 404
+const STATUS_CODE_SERVER_ERROR = 500
+
+
+const jsonOK = function (data, message, metadata) {
+
+    message = (message) ? message : 'Successful request.'
+    metadata = (metadata) ? metadata : {}
+
+    this.status(STATUS_CODE_OK)
+    this.type(TYPE_JSON)
+    return this.json({ message, data, metadata, status: STATUS_CODE_OK })
+
+}
+
+
+const jsonBadRequest = function(data, message, metadata) {
+
+    const status = STATUS_CODE_BAD_REQUEST
+  
+    data = (data) ? data : null
+    message = (message) ? message : 'Bad request.'
+    metadata = (metadata) ? metadata : {}
+  
+    this.status(status)
+    this.type(TYPE_JSON)
+    return this.json({ message, data, metadata, status })
+  }
+  
+  const jsonUnauthorized = function(data, message, metadata) {
+  
+    const status = STATUS_CODE_UNAUTHORIZED
+  
+    data = (data) ? data : null
+    message = (message) ? message : 'Unauthorized.'
+    metadata = (metadata) ? metadata : {}
+  
+    this.status(status)
+    this.type(TYPE_JSON)
+    return this.json({ message, data, metadata, status })
+  }
+  
+  const jsonNotFound = function(data, message, metadata) {
+  
+    const status = STATUS_CODE_NOT_FOUND
+  
+    data = (data) ? data : null
+    message = (message) ? message : 'Not found.'
+    metadata = (metadata) ? metadata : {}
+  
+    this.status(status)
+    this.type(TYPE_JSON)
+    return this.json({ message, data, metadata, status })
+  }
+  
+  const jsonServerError = function(data, message, metadata) {
+  
+    const status = STATUS_CODE_SERVER_ERROR
+  
+    data = (data) ? data : null
+    message = (message) ? message : 'Server error.'
+    metadata = (metadata) ? metadata : {}
+  
+    this.status(status)
+    this.type(TYPE_JSON)
+    return this.json({ message, data, metadata, status })
+  }
+
+  const response = (req, res, next) => {
+
+    res.jsonOK = jsonOK
+    res.jsonBadRequest = jsonBadRequest
+    res.jsonUnauthorized = jsonUnauthorized
+    res.jsonNotFound = jsonNotFound
+    res.jsonServerError = jsonServerError
+  
+    next();
+  };
+  
+  module.exports = response;
