a0d4f1c92b5d8dbfa814c33ab0158a8c6c677ceb
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-14 17:49:09 -0300

    Adicionado Cors na applicação.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const db = require('./models')
 const response = require('./middlewares/response')
 const checkJwt = require('./middlewares/jwt')
@@ -7,6 +8,7 @@ const playerController = require('./controllers/player')
 
 const app = express()
 
+app.use(cors())
 app.use(response)
 app.use(checkJwt)
 app.use(express.json())
