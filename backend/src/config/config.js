9d0e66104fccb5c47f9f62d99d086eff4862124d
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-08-10 19:46:17 -0300

    Adicionado dotenv.

---
+++ backend/src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    operatorsAliases: '0'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+}
