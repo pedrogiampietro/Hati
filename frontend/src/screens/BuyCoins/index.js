3da9e6495854e51ce930ad06cc04a93cd21a03ae
Pedro Giampietro <pedrogiampietro@hotmail.com>
2021-03-30 16:11:38 -0300

    is assigned a value but never used

--- frontend/src/screens/BuyCoins/index.js
+++ frontend/src/screens/BuyCoins/index.js
@@ -37,7 +37,7 @@ const BuyCoins = () => {
   const [wallet, setWallet] = React.useState();
   const [error, setError] = React.useState('');
   const [isShoppingModalOpen, setIsShoppingModalOpen] = React.useState(false);
-  const [pixButtonActive, setPixButtonActive] = React.useState('');
+  const [pixButtonActive] = React.useState('');
   const [qrCode, setQrCode] = React.useState('');
 
   //Create Costumer
