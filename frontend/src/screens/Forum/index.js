2e8319092aa90366d6f0e7b0b7708d743adfe349
Pedro Giampietro <pedrogiampietro@hotmail.com>
2021-05-04 01:26:25 -0300

    removed posts, last posts.

--- frontend/src/screens/Forum/index.js
+++ frontend/src/screens/Forum/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect, useSelector } from 'react-redux';
-import {
-  forumList,
-  forumCreateBoard,
-  setBoardToRemove,
-  boardRemove,
-} from '../../actions/ForumActions';
+import { forumList, forumCreateBoard, setBoardToRemove, boardRemove } from '../../actions/ForumActions';
 import { getToken } from '../../helpers/Account';
 import { getFormData } from '../../helpers/FormData';
 import Container from '../Layouts/Container';
 import InvalidToken from '../../components/Error/InvalidToken';
-import noneAvatar from '../../assets/img/none_avatar.png';
 import { closeModalAvatar } from '../../assets/js/scripts';
 
-const Forum = ({
-  forumList,
-  forumCreateBoard,
-  setBoardToRemove,
-  forumToRemove,
-  boardRemove,
-}) => {
+const Forum = ({ forumList, forumCreateBoard, setBoardToRemove, forumToRemove, boardRemove }) => {
   const { account } = useSelector((state) => state.account);
   const [categoryLists, setCategoryLists] = React.useState([]);
-
   const [postInteraction, setPostInteraction] = React.useState(false);
 
   function interaction() {
@@ -68,11 +55,7 @@ const Forum = ({
       <div className="row">
         <div className="col-xl-12">
           <div className="input-group input-group-lg mb-g">
-            <input
-              type="text"
-              className="form-control shadow-inset-2"
-              placeholder="Search topics"
-            />
+            <input type="text" className="form-control shadow-inset-2" placeholder="Search topics" />
             <div className="input-group-append">
               <span className="input-group-text">
                 <i className="fal fa-search" />
@@ -85,9 +68,7 @@ const Forum = ({
               <div className="card-header bg-white">
                 <div className="row no-gutters align-items-center">
                   <div className="col">
-                    <span className="h6 font-weight-bold text-uppercase">
-                      General
-                    </span>
+                    <span className="h6 font-weight-bold text-uppercase">General</span>
                   </div>
                   {account?.page_access >= 3 ? (
                     <div className="col d-flex">
@@ -105,20 +86,7 @@ const Forum = ({
               </div>
               <div className="row no-gutters row-grid align-items-stretch">
                 <div className="col-12 col-md">
-                  <div className="text-uppercase text-muted py-2 px-3">
-                    Title
-                  </div>
-                </div>
-                <div className="col-sm-6 col-md-2 col-xl-1 hidden-md-down">
-                  <div className="text-uppercase text-muted py-2 px-3">
-                    Status
-                  </div>
-                </div>
-
-                <div className="col-sm-6 col-md-3 hidden-md-down">
-                  <div className="text-uppercase text-muted py-2 px-3">
-                    Last posts
-                  </div>
+                  <div className="text-uppercase text-muted py-2 px-3">Title</div>
                 </div>
               </div>
             </div>
@@ -131,22 +99,12 @@ const Forum = ({
               aria-hidden="true"
               id="newCategory"
             >
-              <div
-                className="modal-dialog modal-dialog-centered modal-transparent"
-                role="document"
-              >
+              <div className="modal-dialog modal-dialog-centered modal-transparent" role="document">
                 <div className="modal-content">
                   <form onSubmit={submitHandler}>
                     <div className="modal-header">
-                      <h4 className="modal-title text-white">
-                        Add new Category
-                      </h4>
-                      <button
-                        type="button"
-                        className="close text-white"
-                        data-dismiss="modal"
-                        aria-label="Close"
-                      >
+                      <h4 className="modal-title text-white">Add new Category</h4>
+                      <button type="button" className="close text-white" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">
                           <i className="fal fa-times" />
                         </span>
@@ -154,10 +112,7 @@ const Forum = ({
                     </div>
                     <div className="modal-body">
                       <div className="form-group">
-                        <label
-                          htmlFor="title"
-                          className="form-label text-white"
-                        >
+                        <label htmlFor="title" className="form-label text-white">
                           Title
                         </label>
                         <input
@@ -170,10 +125,7 @@ const Forum = ({
                       </div>
 
                       <div className="form-group">
-                        <label
-                          htmlFor="description"
-                          className="form-label text-white"
-                        >
+                        <label htmlFor="description" className="form-label text-white">
                           Description
                         </label>
                         <input
@@ -193,10 +145,7 @@ const Forum = ({
                       >
                         Close
                       </button>
-                      <button
-                        type="submit"
-                        className="btn btn-primary waves-effect waves-themed"
-                      >
+                      <button type="submit" className="btn btn-primary waves-effect waves-themed">
                         Add new Category!
                       </button>
                     </div>
@@ -216,16 +165,11 @@ const Forum = ({
                           <div className="p-3">
                             <div className="d-flex">
                               <div className="d-inline-flex flex-column">
-                                <Link
-                                  to={`/forum/${boards.id}`}
-                                  className="fs-lg fw-500 d-block"
-                                >
+                                <Link to={`/forum/${boards.id}`} className="fs-lg fw-500 d-block">
                                   {boards.title}
                                 </Link>
 
-                                <div className="d-block text-muted fs-sm">
-                                  {boards.description}
-                                </div>
+                                <div className="d-block text-muted fs-sm">{boards.description}</div>
                               </div>
                             </div>
                           </div>
@@ -247,45 +191,6 @@ const Forum = ({
                             </div>
                           </div>
                         ) : null}
-
-                        <div className="col-4 col-md-2 col-xl-1 hidden-md-down">
-                          <div className="p-3 p-md-3">
-                            <span className="d-block text-muted">
-                              {boards.topics} <i>Topics</i>
-                            </span>
-                            <span className="d-block text-muted">
-                              {boards.posts} <i>Posts</i>
-                            </span>
-                          </div>
-                        </div>
-                        <div className="col-8 col-md-3 hidden-md-down">
-                          <div className="p-3 p-md-3">
-                            <div className="d-flex align-items-center">
-                              <div className="d-inline-block align-middle status status-success status-sm mr-2">
-                                <img
-                                  src={noneAvatar}
-                                  className="profile-image-md rounded-circle d-block"
-                                  alt=""
-                                />
-                              </div>
-                              <div className="flex-1 min-width-0">
-                                <Link
-                                  to="/last-news"
-                                  className="d-block text-truncate"
-                                >
-                                  Integer id eros vitae leo semper sodales vel a
-                                  est.
-                                </Link>
-                                <div className="text-muted small text-truncate">
-                                  Today, 02:25{' '}
-                                  <Link to="/:name" className="text-info">
-                                    pedro
-                                  </Link>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </div>
                   );
@@ -293,22 +198,13 @@ const Forum = ({
                 {forumToRemove ? (
                   <div className="alert alert-danger rounded shadow-bold col-xl-6 ml-auto mr-auto pl-3 pr-3">
                     <h4 className="alert-heading">Delete Confirmation!</h4>
-                    <p>
-                      Are you sure you want to delete, this action cannot be
-                      undone.
-                    </p>
+                    <p>Are you sure you want to delete, this action cannot be undone.</p>
                     <hr />
                     <div className="d-flex justify-content-between">
-                      <button
-                        className="btn btn-outline-dark"
-                        onClick={cancelDelete}
-                      >
+                      <button className="btn btn-outline-dark" onClick={cancelDelete}>
                         Cancel
                       </button>
-                      <button
-                        className="btn btn-outline-danger"
-                        onClick={confirmDelete}
-                      >
+                      <button className="btn btn-outline-danger" onClick={confirmDelete}>
                         Delete
                       </button>
                     </div>
--- frontend/src/screens/Layouts/Container/index.js
+++ frontend/src/screens/Layouts/Container/index.js
@@ -1,125 +1,118 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-import Header from '../Header'
-import Menu from '../Menu'
-import Footer from '../Footer'
-import { GiWolfHowl } from 'react-icons/gi'
+import Header from '../Header';
+import Menu from '../Menu';
+import Footer from '../Footer';
+import { GiWolfHowl } from 'react-icons/gi';
 
-import { closeMenuOnMobile } from '../../../assets/js/scripts'
+import { closeMenuOnMobile } from '../../../assets/js/scripts';
 
-import './styles.css'
+import './styles.css';
 
 const Container = ({ children }) => {
-	const [title, setTitle] = React.useState('')
-	const location = useLocation()
-	// console.log(location)
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
 
-	React.useEffect(() => {
-		const { pathname } = location
+  React.useEffect(() => {
+    const { pathname } = location;
 
-		switch (pathname) {
-			case '/':
-				setTitle('Home')
-				break
-			case '/sign-in':
-				setTitle('Sign In')
-				break
-			case '/sign-up':
-				setTitle('Sign Up')
-				break
-			case '/highscores':
-				setTitle('Highscores')
-				break
-			case '/account/characters':
-				setTitle('Account Managment')
-				break
-			case '/account/profile':
-				setTitle('Profile')
-				break
-			case '/account/avatar':
-				setTitle('Avatar')
-				break
-			case '/account/characters/create':
-				setTitle('Create Character')
-				break
-			case '/forum':
-				setTitle('Forum')
-				break
-			case '/forum/last-news':
-				setTitle('Last News')
-				break
-			case '/forum/discussions':
-				setTitle('Discussions')
-				break
-			case '/forum/bug-report':
-				setTitle('Bug Report')
-				break
+    switch (pathname) {
+      case '/':
+        setTitle('Home');
+        break;
+      case '/sign-in':
+        setTitle('Sign In');
+        break;
+      case '/sign-up':
+        setTitle('Sign Up');
+        break;
+      case '/highscores':
+        setTitle('Highscores');
+        break;
+      case '/account/characters':
+        setTitle('Account Managment');
+        break;
+      case '/account/profile':
+        setTitle('Profile');
+        break;
+      case '/account/avatar':
+        setTitle('Avatar');
+        break;
+      case '/account/characters/create':
+        setTitle('Create Character');
+        break;
+      case '/forum':
+        setTitle('Forum');
+        break;
+      case '/forum/last-news':
+        setTitle('Last News');
+        break;
+      case '/forum/discussions':
+        setTitle('Discussions');
+        break;
+      case '/forum/bug-report':
+        setTitle('Bug Report');
+        break;
 
-			default:
-				setTitle('Hati AAC')
-		}
-	}, [location])
+      default:
+        setTitle('Hati AAC');
+    }
+  }, [location]);
 
-	return (
-		<div className="mod-bg-1">
-			<div className="page-wrapper">
-				<div className="page-inner">
-					<Menu />
+  return (
+    <div className="mod-bg-1">
+      <div className="page-wrapper">
+        <div className="page-inner">
+          <Menu />
 
-					<div className="page-content-wrapper">
-						<Header />
-						<main id="js-page-content" role="main" className="page-content">
-							<div className="subheader">
-								<h1 className="subheader-title">
-									<GiWolfHowl size={44} color="#886ab5" /> {title}
-								</h1>
-							</div>
+          <div className="page-content-wrapper">
+            <Header />
+            <main id="js-page-content" role="main" className="page-content">
+              <div className="subheader">
+                <h1 className="subheader-title">
+                  <GiWolfHowl size={44} color="#886ab5" /> {title}
+                </h1>
+              </div>
 
-							{children}
+              {children}
 
-							<h3>
-								Hati Team
-								<small className="mb-0">We build cool things...</small>
-							</h3>
-							<div className="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
-								<div className="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
-									<img
-										src="https://i.imgur.com/nZhyuRY.png"
-										alt=""
-										className="img-thumbnail img-responsive rounded-circle"
-										style={{ width: '5rem', height: '5rem' }}
-									/>
-									<div className="ml-2 mr-3">
-										<h5 className="m-0">
-											Developer Web
-											<small className="m-0 fw-300">Lead Author</small>
-										</h5>
-										<a
-											href="https://twitter.com/@pedrogiampietro"
-											className="text-info fs-sm"
-										>
-											@pedrogiampietro
-										</a>{' '}
-										-
-										<span className="text-info fs-sm" title="Contact Sunny">
-											<i className="fal fa-envelope"></i>
-										</span>
-									</div>
-								</div>
-								<div></div>
-							</div>
-						</main>
-						<Footer />
-					</div>
-				</div>
-			</div>
-			<div
-				className="fechaMenu isClose"
-				onClick={() => closeMenuOnMobile()}
-			></div>
-		</div>
-	)
-}
+              <h3>
+                Hati Team
+                <small className="mb-0">We build cool things...</small>
+              </h3>
+              <div className="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
+                <div className="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                  <img
+                    src="https://i.imgur.com/nZhyuRY.png"
+                    alt=""
+                    className="img-thumbnail img-responsive rounded-circle"
+                    style={{ width: '5rem', height: '5rem' }}
+                  />
+                  <div className="ml-2 mr-3">
+                    <h5 className="m-0">
+                      Developer Web
+                      <small className="m-0 fw-300">Lead Author</small>
+                    </h5>
+                    <a href="https://twitter.com/@pedrogiampietro" className="text-info fs-sm">
+                      @pedrogiampietro
+                    </a>{' '}
+                    -
+                    <span className="text-info fs-sm" title="Contact Sunny">
+                      <i className="fal fa-envelope"></i>
+                    </span>
+                  </div>
+                </div>
+                <div></div>
+              </div>
+            </main>
+            <Footer />
+          </div>
+        </div>
+      </div>
+      <div className="fechaMenu isClose" onClick={() => closeMenuOnMobile()}></div>
+    </div>
+  );
+};
 
-export default Container
+export default Container;
